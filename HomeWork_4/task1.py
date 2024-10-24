b0432e453ad4f66e54ef2284d2381868387ef601
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2024-10-24 17:40:26 +0300

    Написал функцию которая отображает аккуратно отформатированный текст. Вариант 1

---
+++ HomeWork_4/task1.py
@@ -0,0 +1,11 @@
+def get_formatted_text():
+    '''Возвращает аккуратно отформатированный текст'''
+    formatted_text = (f"'Dont compare yourself with anyone in this word...\n"
+                      f"if you do so, you are insulting yourself.'\n"
+                      f"\t\t\t\t\t\t Bill Gates\n")
+    return formatted_text
+
+
+print(get_formatted_text())
+
+
