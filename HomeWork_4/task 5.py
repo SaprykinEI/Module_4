67a3974313fff2b72153435a5548e04d731089b4
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2024-10-24 19:12:25 +0300

    Функция возвращает произведение чисел

---
+++ HomeWork_4/task 5.py
@@ -0,0 +1,25 @@
+
+def make_product_number(one_num, two_num):
+    '''Функция возвращает произведение чисел'''
+    try:
+        if 5 <= one_num <= 25 and 6 <= two_num <=25 and one_num < two_num:
+            prod_num = one_num * two_num
+
+        elif 5 <= one_num <= 25 and 5 <= two_num <=25 and one_num > two_num:
+            three_num = one_num
+            one_num = two_num
+            two_num = three_num
+
+            print(one_num, two_num) # Проверка изменений
+            prod_num = one_num * two_num
+
+        else:
+            print("Введите числа в диапозоне от 5 до 25")
+
+        return prod_num
+
+    except Exception:
+        print("Введите числа в диапозоне от 5 до 25")
+
+prod_num = make_product_number(6, 5)
+print(f"Произведение чисел равно {prod_num}")
