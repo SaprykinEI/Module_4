4674e71673e86e2402a65cd70313f2b80b7871e5
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2024-10-24 18:16:44 +0300

    Добавили все чётные числа в список

--- HomeWork_4/task 2.py
+++ HomeWork_4/task 2.py
@@ -1,14 +1,13 @@
-
-
 def displays_even_number(number_one, number_two):
     '''Функция принимает два числа и отображает чётные числа между ними'''
     for even in range(number_one, number_two):
         if even % 2 == 0:
-            print(even)
+            even_numbers.append(even)
+    return even_numbers
 
 
-
-displays_even_number(3, 23)
-
+even_numbers = []
+displays_even_number(3, 25)
+print(even_numbers)
 
 
